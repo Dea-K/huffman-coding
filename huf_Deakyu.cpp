f70044431c1bc9c2f9d64c709478401c7463a157
Dea-k <lee5250@fredonia.edu>
2016-11-26 17:37:58 -0500

    seperated stack as header and implementation files

--- huf_Deakyu.cpp
+++ huf_Deakyu.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <fstream>
 #include <stdlib.h>
+#include "stack.h"
 using namespace std;
 
 const int NUM_ALL_CHAR = 255;
@@ -21,31 +22,8 @@ struct hufNode
 	hufNode *right;
 };
 
-struct tableType
-{
-	char ch;
-	int *binRep;
-	int repSize;
-};
 
-class Stack
-{
-private:
-	tableType *data;
-	int maxSize;
-	int top;
-	int count;
-public:
-	Stack(int);
-	~Stack();
-	bool isEmpty();
-	bool isFull();
-	void push(tableType);
-	void pop(tableType&);
-	void stackTop(tableType&);
-	void printStack();
-	int size();
-};
+
 
 class MinHeap
 {
@@ -363,87 +341,3 @@ void MinHeap::print(int arr[], int n)
 
 
 
-//////////////////////////////////////////////////////////////////
-/////////////  STACK	//////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-
-Stack::Stack(int size)
-{
-	maxSize = size;
-	count = 0;
-	top = -1;
-	data = new tableType[maxSize];
-}
-
-Stack::~Stack()
-{
-	delete[]data;
-}
-
-void Stack::push(tableType indata)
-{
-	if (count < maxSize)
-	{
-		count++;
-		top++;
-		data[top] = indata;
-	}
-	else
-	{
-		cout << "Stack is full..." << endl;
-		return;
-	}
-}
-
-void Stack::pop(tableType &outdata)
-{
-	if (count > 0)
-	{
-		count--;
-		outdata = data[top];
-		top--;
-	}
-	else
-	{
-		cout << "Stack is Empty..." << endl;
-		return;
-	}
-}
-
-void Stack::stackTop(tableType &outdata)
-{
-	if (count > 0)
-	{
-		outdata = data[top];
-	}
-	else
-	{
-		cout << "Stack is Empty..." << endl;
-		return;
-	}
-}
-
-bool Stack::isEmpty()
-{
-	return (count<1);
-}
-
-bool Stack::isFull()
-{
-	return (count>maxSize - 1);
-}
-
-int Stack::size()
-{
-	return count;
-}
-
-void Stack::printStack()
-{
-	cout << "My Stack is: " << endl;
-	for (int i = size() - 1; i >-1; i--)
-	{
-		cout << data[i].ch << ":" << data[i].repSize << endl;
-	}
-	cout << endl;
-}
---
+++ stack.h
@@ -0,0 +1,26 @@
+struct tableType
+{
+	char ch;
+	int *binRep;
+	int repSize;
+};
+
+
+class Stack
+{
+private:
+	tableType *data;
+	int maxSize;
+	int top;
+	int count;
+public:
+	Stack(int);
+	~Stack();
+	bool isEmpty();
+	bool isFull();
+	void push(tableType);
+	void pop(tableType&);
+	void stackTop(tableType&);
+	void printStack();
+	int size();
+};
