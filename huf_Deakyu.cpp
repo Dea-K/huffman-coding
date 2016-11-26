5c83a84f9141e0091eb97959122a85681b2c9262
Dea-k <lee5250@fredonia.edu>
2016-11-25 20:16:01 -0500

    Added first source file

---
+++ huf_Deakyu.cpp
@@ -0,0 +1,20 @@
+// File Name: huf_Deakyu.cpp
+// Author	: Deakyu Lee
+// Created	: 11/25/2016
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <iostream>
+using namespace std;
+
+struct binNode
+{
+
+};
+
+#endif
+int main()
+{
+	system("pause");
+	return 0;
+}
